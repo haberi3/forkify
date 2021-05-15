f6c460d8dd7375e6b05ca18f5e0a79083f108a08
haberi3 <ivan.haber3@gmail.com>
2021-05-15 17:07:46 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -117,6 +117,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the app!");
+};
+
 const init = function () {
   bookmarkView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -125,6 +129,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
 // window.addEventListener("hashchange", showRecipe);
